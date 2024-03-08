b3d9292fcd28967736af7cf57adb7e232a20978a
lfaguilarn <luisfernandoaguilar56@gmail.com>
2024-03-08 15:13:37 -0500

    add dependences
    add dependences

--- src/main/java/Coneccion/Datos.java
+++ src/main/java/Coneccion/Datos.java
@@ -18,17 +18,16 @@ public class Datos {
     String usuario="postgres";
     String contra="21061993";
     String classname = "org.postgresql.Driver";
-    URL url = null;
+
     try{        
-        url = Class.forName("org.postgresql.Driver").getResource("/" + classname.replace('.', '/') + ".class");
-        System.out.println("La ubicación de " + classname + " es: " + url);
-        Class.forName("jar:file:/C:/Users/Luis%20Fernando/.m2/repository/org/postgresql/postgresql/42.6.1/postgresql-42.6.1.jar!/org/postgresql/Driver.class");
+        Class.forName(classname);
         //conector = DriverManager.getConnection(BD,usuario,contra);
         conector = DriverManager.getConnection(BD,usuario,contra);
+        
         System.out.println("conectar "+ param);
         //JOptionPane.showMessageDialog(null,"Coneccion exitosa");
     } catch(Exception e){
-        System.out.println("La ubicación de " + classname + " es: " + url);
+        //System.out.println("-"+conector.getClass().getResource("/" + classname.replace('.', '/') + ".class"));
         System.out.println("Error al conectar: "+e);
         //JOptionPane.showMessageDialog(null,"Error al conectar: "+e);
     }
